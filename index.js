00c5a1d7a36335ef0fb43e17cba9d4bd4d566dd0
Brandon Benson <brandonbenson09@gmail.com>
2022-06-26 20:52:14 -0400

    added prompts

--- index.js
+++ index.js
@@ -1,2 +1,221 @@
-// const inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const db = require("./db/connection");
+
+const start = () => {
+  inquirer
+    .prompt({
+      name: "chooseAction",
+      type: "list",
+      message:
+        "Welcome to YOUR employee tracking system. Would you like to [VIEW], [ADD], [END]",
+    })
+    .then((response) => {
+      // View selection
+      if (response.chooseAction === "VIEW") {
+        inquirer
+          .prompt({
+            name: "chooseView",
+            type: "list",
+            message: "What would you like to view?",
+            choices: ["Departments", "Roles", "Employees"],
+          })
+          .then((response) => {
+            // View DEPARTMENTS
+            if (response.chooseView === "Departments") {
+              connection.query("SELECT * FROM department", (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                start();
+              });
+            }
+            // view ROLES
+            if (response.chooseView === "Roles") {
+              connection.query("SELECT * FROM Roles", (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                start();
+              });
+            }
+            // view EMPLOYEES
+            if (response.chooseView === "Employees") {
+              connection.query("SELECT * FROM employee", (err, res) => {
+                if (err) throw err;
+                console.table(res);
+                start();
+              });
+            }
+          });
+      }
+
+      // add to employee
+      if (response.chooseAction === "ADD") {
+        inquirer
+          .prompt({
+            name: "chooseAdd",
+            type: "list",
+            message: "What would you like to add to?",
+            choices: ["Departments", "Roles", "Employees"],
+          })
+          .then((response) => {
+            // chooses to add Departments
+            if (response.chooseAdd === "Departments") {
+              inquirer
+                .prompt({
+                  type: "input",
+                  message: "Please enter the department name",
+                  name: "deptName",
+                })
+                .then((response) => {
+                  connection.query(
+                    `INSERT INTO department (name) VALUE (?)`,
+                    [response.deptName],
+                    (err, res) => {
+                      if (err) throw err;
+                      connection.query(
+                        "SELECT * FROM department",
+                        (err, res) => {
+                          if (err) throw err;
+                          console.table(res);
+                          start();
+                        }
+                      );
+                    }
+                  );
+                });
+            }
+
+            //Choose to add roles
+            if (response.chooseAdd === "Roles") {
+              inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "Please enter new the roles's title.",
+                    name: "roleTitle",
+                  },
+                  {
+                    type: "input",
+                    message: "Please enter the role's salary.",
+                    name: "roleSalary",
+                  },
+                  {
+                    type: "list",
+                    message: "Please enter the roles's department ID.",
+                    name: "roleId",
+                    choices: [1, 2, 3, 4, 5, 6, 7],
+                  },
+                ])
+                .then((response) => {
+                  connection.query(
+                    "INSERT INTO role ( title, salary, department_id) VALUE (?,?,?)",
+                    [response.roleTitle, response.roleSalary, response.roleId],
+                    (err, res) => {
+                      if (err) throw err;
+                      console.table(res);
+                      connection.query("SELECT * from role", (err, res) => {
+                        if (err) throw err;
+                        console.table(res);
+                        start();
+                      });
+                    }
+                  );
+                });
+            }
+            //Choose to add employees
+            if (response.chooseAdd === "Employees") {
+              inquirer
+                .prompt([
+                  {
+                    type: "input",
+                    message: "What is your employee'ss first name?",
+                    name: "firstName",
+                  },
+                  {
+                    type: "input",
+                    message: "What is your employee'ss last name?",
+                    name: "lastName",
+                  },
+                  {
+                    type: "input",
+                    message: "What is your employee'ss role ID?",
+                    name: "employeeRoleId",
+                  },
+                  {
+                    type: "input",
+                    message: "What is your employee's manager ID",
+                    name: "employeeManagerId",
+                  },
+                ])
+                .then((response) => {
+                  connection.query(
+                    "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUE (?,?,?,?)",
+                    [
+                      response.firstName,
+                      response.lastName,
+                      response.employeeRoleId,
+                      response.employeeManagerId,
+                    ],
+                    (err, res) => {
+                      if (err) throw err;
+                      console.table(res);
+                      connection.query("SELECT * FROM employee", (err, res) => {
+                        if (err) throw err;
+                        console.table(res);
+                        start();
+                      });
+                    }
+                  );
+                });
+            }
+          });
+      }
+      // choose UPDATE
+      if (response.chooseAction === "UPDATE") {
+        inquirer
+          .prompt([
+            {
+              name: "chooseUpdate",
+              type: "list",
+              message: "What would you like to update?",
+              choices: ["Departments", "Roles", "Employees"],
+            },
+          ])
+          .then((response) => {
+            // when DEPARTMENT was chosen to update
+            if (response.chooseUpdate === "Departments") {
+              inquirer
+                .prompt([
+                  {
+                    type: "list",
+                    message: "What department name would you like to update?",
+                    choices: ["Sales", "Legal", "Finance", "Engineering"],
+                    name: "chooseDepartment",
+                  },
+                  {
+                    type: "input",
+                    message: "Please enter the desired update.",
+                    name: "enterDeptChange",
+                  },
+                ])
+                .then((response) => {
+                  connection.query(
+                    "UPDATE department SET name = ? WHERE name = ?",
+                    [response.enterDeptChange, response.chooseDepartment],
+                    (err, res) => {
+                      if (err) throw err;
+                      console.log("Update successful!");
+                    }
+                  );
+
+                  connection.query("SELECT * from department", (err, res) => {
+                    if (err) throw err;
+                    console.table(res);
+                    start();
+                  });
+                });
+            }
+            // when ROLES was chosen to update
+          });
+      }
+    });
+};
